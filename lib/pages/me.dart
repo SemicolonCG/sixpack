84e99a32a473b8f983be7e9a5fb00eaa5dcb3fae
dz <dasuneranda@gmail.com>
2021-06-09 17:21:06 +0530

    calender

--- lib/pages/me.dart
+++ lib/pages/me.dart
@@ -10,8 +10,32 @@ class Me extends StatefulWidget {
 class _MeState extends State<Me> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("me"),
+    return ListView(
+      children: <Widget>[
+        ListTile(
+          leading: Icon(Icons.map),
+          title: Text('Map'),
+        ),
+        ListTile(
+          leading: Icon(Icons.photo_album),
+          title: Text('Album'),
+        ),
+        ListTile(
+          leading: Icon(Icons.phone),
+          title: Text('Phone'),
+        ),
+        ListTile(
+          leading: Icon(Icons.contacts),
+          title: Text('Contact'),
+        ),
+        ListTile(
+          leading: Icon(Icons.settings),
+          title: Text('Setting'),
+        ),
+        Center(
+          child: Text('Version 1.0.0'),
+        ),
+      ],
     );
   }
 }
--- lib/pages/reports.dart
+++ lib/pages/reports.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:sixpack/subpages/records.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 class Reports extends StatefulWidget {
   const Reports({Key? key}) : super(key: key);
@@ -8,10 +10,56 @@ class Reports extends StatefulWidget {
 }
 
 class _ReportsState extends State<Reports> {
+  List<_WeightData> data = [
+    _WeightData('Jan', 150),
+    _WeightData('Feb', 100),
+    _WeightData('Mar', 105),
+    _WeightData('Apr', 100),
+    _WeightData('May', 90),
+    _WeightData('June', 80)
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Reports"),
-    );
+    return Column(children: [
+      //totoal bar
+
+      //history records
+
+      TextButton(
+        onPressed: () => Navigator.push(
+          context,
+          MaterialPageRoute(builder: (_) => Records()),
+        ),
+        child: Text('RECORDS'),
+      ),
+
+      //Initialize the chart widget
+      SfCartesianChart(
+          primaryXAxis: CategoryAxis(),
+          // Chart title
+          title: ChartTitle(text: 'Half yearly weight analysis'),
+          // Enable legend
+          legend: Legend(isVisible: true),
+          // Enable tooltip
+          tooltipBehavior: TooltipBehavior(enable: true),
+          series: <ChartSeries<_WeightData, String>>[
+            LineSeries<_WeightData, String>(
+                dataSource: data,
+                xValueMapper: (_WeightData weigh, _) => weigh.month,
+                yValueMapper: (_WeightData weigh, _) => weigh.weight,
+                name: 'Weight',
+
+                // Enable data label
+                dataLabelSettings: DataLabelSettings(isVisible: true))
+          ]),
+    ]);
   }
+}
+
+class _WeightData {
+  _WeightData(this.month, this.weight);
+
+  final String month;
+  final double weight;
 }
